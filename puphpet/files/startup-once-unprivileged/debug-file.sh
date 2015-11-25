a2d2906dc71fde03507e05005230ef0d74ae18c7
Andrzej Piszczek <andrzej.piszczek@bluepaprica.net>
2015-11-25 14:14:09 +0100

    Change location for debug.sh

--- puphpet/files/startup-once-unprivileged/debug-file.sh
+++ puphpet/files/startup-once-unprivileged/debug-file.sh
@@ -1,4 +1,4 @@
-cat <<EOF > /home/andrzej/debug.sh
+cat <<EOF > /home/vagrant/debug.sh
  if [ \$1 = 'off' ]; then
      sudo sed -i.bak s/zend_extension/\;zend_extension/g /etc/php5/mods-available/zzzz_custom.ini;
  elif [ \$1 = 'on' ]; then
@@ -8,5 +8,5 @@ cat <<EOF > /home/andrzej/debug.sh
  sudo service php5-fpm restart
 EOF
 
-sudo ln -s ~/debug.sh /usr/bin/debug 
+sudo ln -s ~/debug.sh /usr/bin/debug
 sudo chmod 777 /usr/bin/debug
